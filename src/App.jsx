f0481067fcbf9f8751986903b0ea50eaa4fa66f1
Kripesh <kg9773092+github.com>
2025-07-26 10:36:44 +0545

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
+
+function App() {
+  return (
+    <main>
+      <div>
+        <h2 className="text-9xl">GTA</h2>
+      </div>
+    </main>
+  );
+}
+
+export default App;
